99cff1068ac7f1378cf81226e53d51194e5db879
Paola Pasallo <ppasallo@hotmail.com>
2025-04-03 20:23:26 -0300

    TP 3 se agregagan actividades 5 a 7

--- 03 Estructuras Condicionales/PASALLO_TP-03.py
+++ 03 Estructuras Condicionales/PASALLO_TP-03.py
@@ -40,4 +40,49 @@ elif edad >= 18 and edad <30:
 else:
     print("Adulto")
 
+##Actividad 5
+
+def verificar_contraseña(contraseña):
+    if 8<= len(contraseña) <= 14:
+        print("Ha ingresado una contraseña correcta")
+    else:
+        print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres")
+
+contraseña = input("Ingrese su contraseña:")
+verificar_contraseña(contraseña)
+
+##Actividad 6
+
+from statistics import mode, median, mean
+import random
+numeros_aleatorios = [random.randint(1, 100) for i in range(50)]
+
+moda = mode(numeros_aleatorios)
+mediana = median(numeros_aleatorios)
+media = mean(numeros_aleatorios)
+
+if media > mediana:
+    print("Hay sesgo positivo")
+elif media < mediana:
+    print("Hay sesgo negativo")
+else:
+    print("No hay sesgo")
+
+print(f"Moda: {moda}")
+print(f"Mediana: {mediana}")
+print(f"Media: {media}")
+
+##Actividad 7
+
+texto = input("Introduce una frase o palarabra:")
+
+if texto [-1].lower() in 'aeiou':
+    texto+= '!'
+    print(texto)
+else:
+    print(texto)
+
+##Actividad 8
+
+
 
